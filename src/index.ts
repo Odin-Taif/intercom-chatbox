6bbde05c01adaa2e1ac1b457606865cc755596ba
Odin Thif <mjd.thif@icloud.com>
2025-02-28 14:16:59 +0100

    removed .

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from "./app";
 
 createApp().listen(3001, "0.0.0.0", () => {
-  console.log(`Server started on http://0.0.0.0:3001.`);
+  console.log(`Server started on http://0.0.0.0:3001`);
 });
