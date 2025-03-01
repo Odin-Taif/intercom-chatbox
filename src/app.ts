ef11de4342b663a8da7d2321187ea2ff0b582b0c
Odin Thif <mjd.thif@icloud.com>
2025-03-01 13:59:36 +0100

    deployement

--- src/app.ts
+++ src/app.ts
@@ -14,35 +14,22 @@ export function createApp() {
   app.use(express.static(path.join(__dirname)));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(cookieParser());
   app.use(express.json());
-
-  app.get("/", (req, res) => {
+  app.get("/status", (req, res) => {
     res.json({ status: "ready" });
   });
-
-  // const v1Router = Router();
-  // v1Router.use("/", postFeature.router);
-  // v1Router.use("/", userFeature.router);
-
-  // const apiRouter = Router();
-  // apiRouter.use("/api/v1", v1Router);
-
-  // app.use("/", apiRouter);
-
-  // app.use("/", apiRouter);
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "index.html"));
-  // });
-  // app.post("/initialize", (request, response) => {
-  //   response.send(initialCanvas);
-  // });
-  // app.post("/submit", (request, response) => {
-  //   if (request.body.component_id == "submit_button") {
-  //     response.send(finalCanvas);
-  //   } else {
-  //     response.send(initialCanvas);
-  //   }
-  // });
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  app.post("/initialize", (request, response) => {
+    response.send(initialCanvas);
+  });
+  app.post("/submit", (request, response) => {
+    if (request.body.component_id == "submit_button") {
+      response.send(finalCanvas);
+    } else {
+      response.send(initialCanvas);
+    }
+  });
   return app;
 }
